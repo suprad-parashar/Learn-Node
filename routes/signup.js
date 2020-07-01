bccb55e5d99a8f638b15c074203555fa4bdea14c
sandy13521 <nandavarmukund1999gmail.com>
2020-07-01 21:46:38 +0530

    Added Registration Page

--- routes/signup.js
+++ routes/signup.js
@@ -18,34 +18,66 @@ router.post("/", (request, response) => {
     let name = request.body.name;
     let email = request.body.email;
     let pass = request.body.pass;
-    let re_pass = request.body.re_pass;
-    if (pass !== re_pass) {
-        response.sendFile(path.resolve("./views/html/signup.html"), {
-            validationMessage: "Password Mismatch at server."
-        });
-    }
-    firebase.auth().createUserWithEmailAndPassword(email, pass).then(() => {
-        let user = firebase.auth().currentUser;
-        database.ref().child("users").child(user.uid).child("data").set({
-            points: 100,
-            moderator: false,
-            type: "Student"
-        });
-        user.updateProfile({
-            displayName: name
-        });
-        user.sendEmailVerification().then(() => {
-            console.log('Verification Mail Has been sent Please Verify Before you login.');
-            window.location.replace('/login');
+    console.log(name);
+    console.log(email);
+
+    try {
+        firebase.auth().createUserWithEmailAndPassword(email, pass).then(() => {
+            let database = firebase.database();
+            let user = firebase.auth().currentUser;
+            database.ref().child("users").child(user.uid).child("data").set({
+                points: 100,
+                moderator: false,
+                type: "Student"
+            }).catch(error => {
+                console.log(error.message);
+            });
+            user.updateProfile({
+                displayName: name
+            }).catch(error => {
+                response.send("<h1>Firebase User Profile Cannot be updated</h1>");
+            });
+            user.sendEmailVerification().then(() => {
+                console.log('Verification Mail Has been sent Please Verify Before you login.');
+            }).catch(error => {
+                console.log(error.message);
+            });
         }).catch(error => {
-            console.log("Error Sending Verification Email");
-            console.log(error.message);
+            response.send("<h1>Email Already Exists!</h1>");
         });
-        firebase.auth().signOut();
+    } catch (error) {
+        console.log(error.message);
+    }
+    firebase.auth().signOut().then(function () {
+        response.send("<h1>Success</h1>");
     }).catch(error => {
-        response.send("<h1>Email Already Exists!</h1>");
+        response.send("<h1>Logout failed</h1>");
     });
-});
+})
+
+//
+//     firebase.auth().createUserWithEmailAndPassword(email, pass).then(() => {
+//         let user = firebase.auth().currentUser;
+//         database.ref().child("users").child(user.uid).child("data").set({
+//             points: 100,
+//             moderator: false,
+//             type: "Student"
+//         });
+//         user.updateProfile({
+//             displayName: name
+//         });
+//         user.sendEmailVerification().then(() => {
+//             console.log('Verification Mail Has been sent Please Verify Before you login.');
+//             window.location.replace('/login');
+//         }).catch(error => {
+//             console.log("Error Sending Verification Email");
+//             console.log(error.message);
+//         });
+//         firebase.auth().signOut();
+//     }).catch(error => {
+//         response.send("<h1>Email Already Exists!</h1>");
+//     });
+// });
 
 //Export Router.
 module.exports = router;
