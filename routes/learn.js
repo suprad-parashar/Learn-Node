66090228c85651bb85691de9e46f00c9f5c1ffea
sandy13521 <nandavarmukund1999gmail.com>
2020-07-08 20:23:50 +0530

    Adding Learn

--- routes/learn.js
+++ routes/learn.js
@@ -3,7 +3,6 @@ const firebase = require("../firebase");
 const express = require("express");
 const path = require("path");
 const helper = require("../helper");
-let List = require("collections/list");
 
 //Create Router Object.
 const router = express.Router();
