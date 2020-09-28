e7fc543674def6f4ec5126c35b87a7417e65bee5
Suprad Parashar <suprad.s.parashar@gmail.com>
2020-09-28 11:55:04 +0530

    Fixed Activity add bug

--- routes/courses.js
+++ routes/courses.js
@@ -29,21 +29,32 @@ router.get("/:course/video/:resource", (request, response) => {
     database.ref().child("links").child(fcourse).child("Videos").child(resource).once('value').then(function (snapshot) {
         let reference = database.ref().child("users").child(user.uid).child("activity");
         reference.once("value").then(activity => {
-            let reference = database.ref().child("users").child(user.uid).child("activity");
-            let date = new Date().toDateString().split(" ");
-            let dateString = date[2] + " " + date[1] + ", " + date[3];
-            reference.child(activity.numChildren().toString()).update({
-                date: dateString,
-                done: false,
-                from: snapshot.child("from").val(),
-                index: 0,
-                link: snapshot.child("link").val(),
-                name: snapshot.child("name").val(),
-                playlist: snapshot.child("playlist").val(),
-                reference: snapshot.ref.toString(),
-                time: 0,
-                type: "VIDEO"
-            });
+            let last = activity.numChildren();
+            let exists = false;
+            for (let i = 1; i < 3; i++) {
+                if (activity.child((last - i).toString()).child("name").val() === snapshot.child("name").val()) {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) {
+                let date = new Date().toDateString().split(" ");
+                let dateString = date[2] + " " + date[1] + ", " + date[3];
+                reference.child(activity.numChildren().toString()).update({
+                    date: dateString,
+                    done: false,
+                    from: snapshot.child("from").val(),
+                    index: 0,
+                    link: snapshot.child("link").val(),
+                    name: snapshot.child("name").val(),
+                    playlist: snapshot.child("playlist").val(),
+                    reference: snapshot.ref.toString(),
+                    time: 0,
+                    type: "VIDEO"
+                }).catch(error => {
+                    console.log(error.message);
+                });
+            }
         }).catch(error => {
             console.log(error.message)
         });
